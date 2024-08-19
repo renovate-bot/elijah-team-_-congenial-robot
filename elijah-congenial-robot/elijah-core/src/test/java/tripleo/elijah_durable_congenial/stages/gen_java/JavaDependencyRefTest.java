fcaab4a6791e8a282d22b857e9c787cf17f0c0a1
Tripleo <molasses@tripleo.milkyway>
2024-08-19 03:53:37 -0400

    Disable JavaDependencyRefTest

--- elijah-congenial-robot/elijah-core/src/test/java/tripleo/elijah_durable_congenial/stages/gen_java/JavaDependencyRefTest.java
+++ elijah-congenial-robot/elijah-core/src/test/java/tripleo/elijah_durable_congenial/stages/gen_java/JavaDependencyRefTest.java
@@ -1,27 +1,22 @@
 package tripleo.elijah_durable_congenial.stages.gen_java;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 public class JavaDependencyRefTest {
-	JavaDependencyRef jdr;
-
-	@Before
-	public void setUp() {
-		jdr = new JavaDependencyRef();
-		jdr.setClassName("className");
-		jdr.setFieldName("fieldName");
-		jdr.setPackageName("packageName");
-	}
-
-	@Test
-	public void jsonString() {
-		final String jsonString = jdr.jsonString();
-
-		System.err.println(jsonString);
-
-		assertEquals("{\"className\":\"className\",\"fieldName\":\"fieldName\",\"packageName\":\"packageName\"}", jsonString);
-	}
+//	JavaDependencyRef jdr;
+//
+//	@Before
+//	public void setUp() {
+//		jdr = new JavaDependencyRef();
+//		jdr.setClassName("className");
+//		jdr.setFieldName("fieldName");
+//		jdr.setPackageName("packageName");
+//	}
+//
+//	@Test
+//	public void jsonString() {
+//		final String jsonString = jdr.jsonString();
+//
+//		System.err.println(jsonString);
+//
+//		assertEquals("{\"className\":\"className\",\"fieldName\":\"fieldName\",\"packageName\":\"packageName\"}", jsonString);
+//	}
 }
